f55f6950296af477967ccada3c6a739690ed7110
anniewanjohi <annieww24@gmail.com>
2025-04-26 11:54:57 +0300

    commit 3

--- loop_factorial.py
+++ loop_factorial.py
@@ -1,6 +1,8 @@
 #computing the factorial of a number using for loop
 num =int(input("Enter an integer number:"))
+#intializing the variable fact to 1
 fact=1
+#range -creates a sequence from 1 upto the number
 for i in range(1,num+1):
     fact=fact*i
 print("The factorial of",num,"is",fact)
