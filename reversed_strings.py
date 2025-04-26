118c280350556997541887441c1e3e005e490cfa
anniewanjohi <annieww24@gmail.com>
2025-04-26 19:19:10 +0300

    commit 4

--- reversed_strings.py
+++ reversed_strings.py
@@ -1,7 +1,10 @@
-string=input("Enter a string:")
+def reverse_sring(string):
 #initializing an empty string
-reversed_string=""
-#looping through the string from the last char to the first
-for i in range(len(string)-1,-1,-1):
-    reversed_string+=string[i]
-print("Reversed string:",reversed_string)
+    reversed_string=""
+      #looping through the string from the last char to the first
+    for i in range(len(string)-1,-1,-1):
+      reversed_string+= string[i]
+    return reversed_string
+user_input=input("Enter  a string:")
+new_string=reverse_sring(user_input)
+print("Reversed string:",new_string)
